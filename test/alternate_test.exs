eacb92c230a5256181242bf8a9b3d90e31cb18db
Mohamed Boudra <boudra.moha@gmail.com>
2016-11-13 01:17:24 +0100

    basic plug test

---
+++ test/alternate_test.exs
@@ -0,0 +1,8 @@
+defmodule AlternateTest do
+  use ExUnit.Case
+  doctest Alternate
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
---
+++ test/plug_test.exs
@@ -0,0 +1,19 @@
+defmodule AlternatePlugTest do
+
+  use ExUnit.Case
+  use Phoenix.ConnTest
+
+  defmodule TestGettext do
+    use Gettext, otp_app: :alternate
+  end
+
+  test "plug sets the Gettext locale" do
+    Application.put_env(:alternate, :gettext_module, TestGettext)
+    opts = Alternate.Plug.init(nil)
+    build_conn
+    |> assign(:alternate_locale, "en-US")
+    |> Alternate.Plug.call(opts)
+    assert Gettext.get_locale(TestGettext) == "en-US"
+  end
+
+end
