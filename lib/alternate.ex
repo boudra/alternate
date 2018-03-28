9597fbb5ff835bcd0d0deb3b2c86293d0ba263c4
Mohamed Boudra <boudra.moha@gmail.com>
2018-03-28 14:01:28 +0200

    add standard route with nil locale

--- lib/alternate.ex
+++ lib/alternate.ex
@@ -19,10 +19,21 @@ defmodule Alternate do
     Application.get_env(:alternate, :locale_assign_key, :alternate_locale)
   end
 
+  defp build_options(options, locale) do
+    quote do
+      assigns_with_locale = Map.new([{unquote(locale_assign_key), unquote(locale)}])
+
+      assigns =
+        Keyword.get(unquote(options), :assigns, %{})
+        |> Map.merge(assigns_with_locale)
+
+      Keyword.put(unquote(options), :assigns, assigns)
+    end
+  end
+
   defp do_localize({verb, meta, [path, plug, plug_opts, options]}) do
     locales
     |> Enum.to_list()
-    |> Enum.concat([{nil, %{path_prefix: ""}}])
     |> Enum.map(fn {locale, config} ->
       path =
         quote do
@@ -37,19 +48,13 @@ defmodule Alternate do
           end <> translated_path
         end
 
-      options =
-        quote do
-          assigns_with_locale = Map.new([{unquote(locale_assign_key), unquote(locale)}])
+      options = build_options(options, locale)
 
-          assigns =
-            Keyword.get(unquote(options), :assigns, %{})
-            |> Map.merge(assigns_with_locale)
-
-          Keyword.put(unquote(options), :assigns, assigns)
-        end
-
-      {verb, meta, [path, plug, [action: plug_opts, locale: locale], options]}
+      {verb, meta, [path, plug, [action: plug_opts, locale: locale], build_options(options, nil)]}
     end)
+    |> Enum.concat([
+      {verb, meta, [path, plug, plug_opts, build_options(options, nil)]}
+    ])
   end
 
   defmacro localize({verb, meta, [path, plug, plug_opts]}) when verb in @http_methods do
